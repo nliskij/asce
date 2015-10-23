f84be694a52d06f9680e765abdf1c4027083b519
nlj <nlj@users.noreply.github.com>
2015-10-23 07:09:54 +0000

    Started the project and defined the base of the bitboard
    -This project will use a struct that contains bitboards for every piece of every color

--- main.c
+++ main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#typedef uint64_t bitboard_t
+
+typedef struct board {
+    bitboard_t w_pawns;
+    bitboard_t w_knights;
+    bitboard_t w_bishops;
+    bitboard_t w_rooks;
+    bitboard_t w_queens;
+    bitboard_t w_king;
+    bitboard_t b_pawns;
+    bitboard_t b_knights;
+    bitboard_t b_bishops;
+    bitboard_t b_rooks;
+    bitboard_t b_queens;
+    bitboard_t b_king;
+}
 
 int main(int argc, char *argv[])
 {
